a7887a554b75a1c4ca4b59bf87949d154593fe71
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2025-04-24 00:21:57 -0400

    WIP

--- src/semver.sh
+++ src/semver.sh
@@ -86,7 +86,7 @@ function match_prefix() {
   local expected_prefix=$1
   local line
   while IFS= read -r line; do
-    if [[ (.*)(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))? ]]; then
+    if [[ $line =~ (.*)(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))? ]]; then
       local prefix=${BASH_REMATCH[1]}
       if [[ $prefex == $expected_prefix ]]; then
         echo $line
