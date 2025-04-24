faa205f2729897794fbc0538e46bd6d172dbcb4e
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2025-04-24 04:26:39 -0400

    WIP

---
+++ src/sort.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+function transform() {
+  local expected_prefix=$1
+  local line
+  prefix_length=${#expected_prefix}
+  while IFS= read -r line; do
+    if [[ $line =~ ^(.+)-(.+)$ ]]; then
+        echo "$line_"
+    else
+        echo "$line"
+  done
+}
+
+function my_sort() {
+    transformed=$(transform)
+    echo "$transformed" | sort -V
+}
+
+my_sort
