66acd912e164d12e94dcf1585df2da38a3abfc47
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2025-11-04 06:57:18 -0500

    Correct regex for major version > 9

--- src/filter.sh
+++ src/filter.sh
@@ -8,13 +8,14 @@
 function main() {
   local expected_prefix=$1
   local line
-  prefix_length=${#expected_prefix}
+  local prefix_length=${#expected_prefix}
   while IFS= read -r line; do
-    if [[ $line =~ ^(.*)(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$ ]]; then
-      local prefix=${BASH_REMATCH[1]}
-      if [[ $prefix == $expected_prefix ]]; then
-        echo ${line:$prefix_length}
-      fi
+    if [[ ${#line} -le $prefix_length ]]; then continue
+    fi
+    local prefix=${line:0:$prefix_length}
+    local target=${line:$prefix_length}
+    if [[ $prefix == $expected_prefix && $target =~ ^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$ ]]; then
+      echo $target
     fi
   done
 }
--- src/split.sh
+++ src/split.sh
@@ -9,15 +9,15 @@
 #   - "0 0 0 . ." if input is not a valid semantic version
 function main() {
   local version=$1
-  if [[ $version =~ ^(.*)(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$ ]]; then
-    local major=${BASH_REMATCH[2]}
-    local minor=${BASH_REMATCH[3]}
-    local patch=${BASH_REMATCH[4]}
-    local pre_release=${BASH_REMATCH[6]}
+  if [[ $version =~ ^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$ ]]; then
+    local major=${BASH_REMATCH[1]}
+    local minor=${BASH_REMATCH[2]}
+    local patch=${BASH_REMATCH[3]}
+    local pre_release=${BASH_REMATCH[5]}
     if [[ $pre_release == "" ]]; then
       pre_release="."
     fi
-    local build=${BASH_REMATCH[11]}
+    local build=${BASH_REMATCH[10]}
     if [[ $build == "" ]]; then
       build="."
     fi
